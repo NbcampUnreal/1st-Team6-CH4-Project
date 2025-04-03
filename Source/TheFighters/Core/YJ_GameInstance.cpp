5a16721fac4b299811f6c09fa59ecfe3431a1ca0
erldfd <hseongrae@gmail.com>
2025-04-03 13:05:26 +0900

    블루프린트 C++ 기반으로 상속 받도록 변경
    GameMode, GameInstance, GameState, PlayerState, CharacterBase

---
+++ Source/TheFighters/Core/YJ_GameInstance.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Core/YJ_GameInstance.h"
+
---
+++ Source/TheFighters/Core/YJ_GameState.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "YJ_GameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFIGHTERS_API AYJ_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+};
