c8215cc35b406b9aae188f5865cdfeebc1031f4d
erldfd <hseongrae@gmail.com>
2025-04-13 22:11:36 +0900

    플레이어 식별 UI 버그 수정, vectory 화면 버그 수정 및 기타
    # 플레이어 식별 UI 버그 수정
    - 패키징하고 실행하면 식별 UI 초기화 일어나지 않는 현상 수정
    
    # vectory 화면 버그 수정
    - 클라이언트에서 정상적으로 점수판이 보이지 않는 현상 수정
    
    # 기타
    - 방목록에 우리 게임만 보이도록 수정

--- Source/TheFighters/Character/YJ_PlayerController.h
+++ Source/TheFighters/Character/YJ_PlayerController.h
@@ -1,9 +1,8 @@
-
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+
 #include "YJ_PlayerController.generated.h"
 
 /**
@@ -14,4 +13,11 @@ class THEFIGHTERS_API AYJ_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+
+protected:
+
+	virtual void PostNetInit() override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "YJPlayerController")
+	void OnPostNetInit();
 };
